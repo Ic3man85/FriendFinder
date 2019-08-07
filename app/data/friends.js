58b10a29b3ae8eb5af9e5dce333ddc30769b2e53
Iceman <Zachary.Draper85@gmail.com>
2019-08-07 05:19:58 -0600

    friends.js done, starting the html pages before i continue

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+let friends = [{
+        name: "Jonny",
+        photo: "",
+        scores: [5, 4, 3, 4, 5]
+    },
+    {
+        name: "Steve",
+        photo: "",
+        scores: [1, 4, 3, 2, 2]
+    },
+    {
+        name: "Adam",
+        photo: "",
+        scores: [4, 5, 2, 1, 4]
+    },
+    {
+        name: "Carol",
+        photo: "",
+        scores: [1, 5, 3, 2, 4]
+    },
+    {
+        name: "Amy",
+        photo: "",
+        scores: [4, 3, 2, 2, 1]
+    }
+];
+
+module.exports = friends;
