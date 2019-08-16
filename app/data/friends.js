92cce06a9477825d20ef341dde9b92b73ff3730f
Iceman <Zachary.Draper85@gmail.com>
2019-08-16 16:28:29 -0600

    fixing stuff

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,27 +1,27 @@
 let list = [{
         name: "Jonny",
         photo: "https://m.media-amazon.com/images/M/MV5BMTM0ODU5Nzk2OV5BMl5BanBnXkFtZTcwMzI2ODgyNQ@@._V1_UY317_CR4,0,214,317_AL_.jpg",
-        scores: ["5", "4", "5", "4", "5"]
+        scores: ["5", "4", "5", "4", "5", "5", "2", "3", "1", "3"]
     },
     {
         name: "Steve",
         photo: "https://www.carolines.com/wp-content/uploads/2019/04/29_Edp-2-e1560358821718.png",
-        scores: ["1", "4", "3", "2", "2"]
+        scores: ["1", "4", "3", "2", "2", "5", "3", "4", "3", "2"]
     },
     {
         name: "Adam",
         photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Adam_Sandler_2011_%28Cropped%29.jpg/220px-Adam_Sandler_2011_%28Cropped%29.jpg",
-        scores: ["4", "5", "2", "1", "4"]
+        scores: ["4", "5", "2", "1", "4", "2", "4", "5", "3", "2"]
     },
     {
         name: "Carol",
         photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSLfHwQGSizw9Qd-7o5crdM2JJpR3nZ9z3bwrH2VwzkWSiqn1Zr",
-        scores: ["1", "5", "3", "2", "4"]
+        scores: ["1", "5", "3", "2", "4", "3", "4", "2", "5", "2"]
     },
     {
         name: "Amy",
         photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQT7xsv6s3aDAxAEap5uWjdf75du5a9UTm2FgIo12txT5DFI-5f",
-        scores: ["4", "3", "2", "2", "1"]
+        scores: ["4", "3", "2", "2", "1", "2", "4", "5", "2", "4"]
     },
 ];
 
